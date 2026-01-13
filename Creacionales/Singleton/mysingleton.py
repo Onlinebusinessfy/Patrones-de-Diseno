efa0f4e5a1c46017557a2b150d1d06b55d1cf1f7
Aaron Dominguez <onlinebusinessfy@gmail.com>
2026-01-12 20:57:17 -0800

    Singleton

---
+++ Creacionales/Singleton/mysingleton.py
@@ -0,0 +1,29 @@
+import os
+import uuid
+
+class MySingleton:
+    
+    _instancia = None
+    _id = None
+    
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super(MySingleton, cls).__new__(cls)
+            cls._id = uuid.uuid4()
+        return cls._instancia
+    
+    def getId(self):
+        self._id
+        return self._id
+        
+    def __str__(self):
+        return f"Nombre: {self._nombre}"
+    
+mysingleton1 = MySingleton()
+mysingleton2 = MySingleton()
+
+# print(mysingleton1.getNombre())
+os.system("cls")
+print(mysingleton1.getId())
+print(mysingleton2.getId())
+
